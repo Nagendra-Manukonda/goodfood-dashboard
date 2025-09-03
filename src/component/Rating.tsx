59d24b08078de5dfa5e891f5d08da6c6e8b8fe65
Nagendra Manukonda <nagendra.manukonda@jsspro.com>
2025-09-03 15:40:39 +0530

    rating component edited

--- src/component/Rating.tsx
+++ src/component/Rating.tsx
@@ -53,36 +53,36 @@ function OuterRing({
 
 export default function Rating() {
   return (
-    <div className="relative  flex justify-center items-center h-80 sm:h-72 md:h-60 scale-80 sm:scale-100">
-      <div className="absolute top-10 -right-12 sm:top-8 sm:right-6 md:top-15 md:-right-1">
-        <OuterRing value={70} size={190} color="#F97316">
-          <div
-            className="flex flex-col items-center justify-center bg-[#F97316]/90 w-44 h-44 rounded-full text-white shadow-md"
-           
-          >
-            <span className="text-lg font-bold">85%</span>
-            <span className="text-xs">Food Taste</span>
+    <div
+      className="
+        relative flex justify-center items-center 
+        w-full h-[280px] sm:h-[350px] md:h-[420px]
+        scale-75 sm:scale-90 md:scale-100
+        transition-transform
+      "
+    >
+      <div className="absolute right-10 sm:right-14 ">
+        <OuterRing value={60} size={180} color="#F97316">
+          <div className="flex flex-col items-center justify-center bg-[#F97316]/90 w-40 h-40 rounded-full text-white shadow-md">
+            <span className="text-xl font-bold">85%</span>
+            <span className="text-sm">Food Taste</span>
           </div>
         </OuterRing>
       </div>
 
-      <div className="absolute top-3 -left-5 sm:bottom-9 md:-top-1 md:left-16">
-        <OuterRing value={70} size={125} color="#8B5CF6">
-          <div
-            className="flex flex-col items-center justify-center bg-[#8B5CF6]/80 w-28 h-28 rounded-full text-white shadow-md"
-          >
+      <div className="absolute -top-1 left-15 sm:top-20 sm:left-18 z-10">
+        <OuterRing value={72} size={120} color="#8B5CF6">
+          <div className="flex flex-col items-center justify-center bg-[#8B5CF6]/80 w-24 h-24 rounded-full text-white shadow-md">
             <span className="text-lg font-bold">85%</span>
             <span className="text-xs">Hygiene</span>
           </div>
         </OuterRing>
       </div>
 
-      <div className="absolute bottom-7 -left-17 sm:-bottom-10 sm:left-20 md:-bottom-3 md:-left-6">
-        <OuterRing value={70} size={150} color="#06B6D4">
-          <div
-            className="flex flex-col items-center justify-center bg-[#06B6D4]/90 w-32 h-32 rounded-full text-white shadow-md"
-          >
-            <span className="text-lg font-bold">95%</span>
+      <div className="absolute bottom-6 -left-8 sm:bottom-18 sm:left-1 z-10">
+        <OuterRing value={72} size={140} color="#06B6D4">
+          <div className="flex flex-col items-center justify-center bg-[#06B6D4]/90 w-28 h-28 rounded-full text-white shadow-md">
+            <span className="text-lg font-bold">92%</span>
             <span className="text-xs">Packaging</span>
           </div>
         </OuterRing>
